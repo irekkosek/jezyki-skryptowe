652f4d4a0bcdade97fab258b05bfe2385ff16288
irekkosek <irekkosek.IK@gmail.com>
2023-01-22 19:21:08 +0000

    final project commit

--- projekt/projekt.py
+++ projekt/projekt.py
@@ -1,45 +1,53 @@
 import math
 import sys
-from datetime import date, datetime
+from datetime import datetime
+
 class Algorithm():
     def solution(self, n):
         sum = math.sqrt(n + 1)
         output=""
-        output+="numeric sum: {0}\n".format(sum)
-        output+="symbolic sum: sqrt({0})\n".format(n + 1)
+        output+=f"numeric sum: {sum}\n"
+        output+=f"symbolic sum: sqrt({n+1})\n"
         return output
 
 if __name__ == "__main__":
+    exitcode = 0
+    htmlendl = "<br>\n"
     now = datetime.now()
     fulldate = now.strftime("%d-%m-%Y %H:%M:%S")
-    with open("input", "r") as file_input:
-        with open("output", "w") as file_output:
-            with open("index.html", "w") as file_html:
-                file_html.write("<html><body>")
-                file_html.write("<h1>{0}</h1>".format(fulldate))
-                calculate = Algorithm()
-                try:
-                    for line in file_input:
-                        n = int(line)
-                        print("Enter n:{0}".format(n))
-                        if n < 1:
-                            raise Exception("n must be greater than 0")
-                        output=calculate.solution(n)
-                        output_html=output.replace("\n", "<br>")
-                        file_output.write(output)
-                        file_html.write(output_html)
-                except ValueError:
-                    print("n must be a number")
-                    sys.exit(1)
-                except Exception as e:
-                    print(e)
-                    sys.exit(1)
-                file_html.write("</body></html>")
 
+    file_input = open("input", "r")
+    file_output = open("output", "w")
+    file_html = open("output.html", "w")
 
+    file_html.write("<html><body>")
+    file_html.write(f"<h1>{fulldate}</h1>\n")
 
+    calculate = Algorithm()
 
+    for line in file_input:
+        try:
+            file_html.write(f"Enter n: {line}"+htmlendl)
+            n = int(line)
+            
+            if n < 1:
+                raise Exception("n must be greater than 0")
+            
+            output=calculate.solution(n)
+            output_html=output.replace("\n", htmlendl)
 
+            file_output.write(output)
+            file_html.write(output_html)
+        except ValueError:
+            file_html.write("n must be a number"+htmlendl)
+            exitcode = 1
+        except Exception as e:
+            file_html.write(str(e)+htmlendl)
+            exitcode = 1
+    file_html.write("</body></html>")
 
-    
-    
+    file_input.close()
+    file_output.close()
+    file_html.close()
+
+    sys.exit(exitcode)
